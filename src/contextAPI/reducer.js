da2a9d7347c0226b759f214f620eaad91ce505a5
Truonghq97 <truonghq97@gmail.com>
2020-10-02 21:00:10 +0700

    add component checkout & subtotal

--- src/contextAPI/reducer.js
+++ src/contextAPI/reducer.js
@@ -1,25 +1,46 @@
 export const initialState = {
-    basket: [],
-    user: null,
-}
+  basket: [],
+  user: null,
+};
 
-const reducer = (state = initialState, action) => {
-    console.log(action);
-    switch(action.type) {
-        case "ADD_TO_BASKET":
-            // logic for adding item to basket
-            return {
-                ...state,
-                basket: [...state.basket, action.item]
-            };
-        case "REMOVE_FROM_BASKET":
-            // Logic for remove item to basket
-            return {
-                ...state
-            };
-        default:
-            return state;
-    }
-}
+export const getBasketTotal = (basket) =>
+  basket.reduce((amount, item) => item.price + amount, 0);
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      // logic for adding item to basket
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+    case "REMOVE_FROM_BASKET":
+      // Logic for remove item to basket...
+
+      // We cloned the basket
+      let newBasket = [...state.basket];
+
+      // We check to see if product exists,
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      if (index >= 0) {
+        // item exists in basket, remove it...
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.id}) as its not in basket!`
+        );
+      }
+      return {
+        ...state,
+        basket: newBasket,
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
